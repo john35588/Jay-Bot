fe9c074e1d46d891e6e6cc1dae9e7b882ccd33da
john35588 <56525183+john35588@users.noreply.github.com>
2019-10-15 21:02:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var respond
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/Hi Jay!$/;
+      botRegex = /^\/Hi Jay$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
