674c7f6a6cf888e703d86896857a635e95f19a15
john35588 <56525183+john35588@users.noreply.github.com>
2019-10-20 15:52:21 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,162 +3,33 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
-var helpMessageA ="noahgula.me/heman/help.html";
-
-function help() {
-    return helpMessageA;
-}
-
-function whatsup() {
-    var message = "https://www.youtube.com/watch?v=ZZ5LpwO-An4";
-    return message;
-}
-
-function secret() {
-    var message = cool();
-    return message;
-}
-
-function lion() {
-    var message = "https://www.youtube.com/watch?v=_LBmUwi6mEo";
-    return message;
-}
-
-function channel() {
-    var message = "https://www.youtube.com/watch?v=E9s1ltPGQOo";
-    return message;
-}
-
-function shop() {
-    var message = "https://www.youtube.com/watch?v=gYOEyzBFYa4";
-    return message;
-}
-
-/* function holes() {
-    var num = Math.floor(Math.random() * (3 - 0)) + 0;
-    if(num===0) {
-      var message = ("A straw has no holes.");
-    }
-    else if(num===1) {
-      var message = ("A straw has one hole.");
-    }
-    else if(num===2) {
-      var message = ("A straw has two holes.");
-    }
-    return message;
-} */
-
-function holes() {
-    var message = "I don't even know, man.\n";
-    var HTTPS = require('https');
-    var cool = require('cool-ascii-faces');
-
-    var botID = process.env.BOT_ID;
-
-    var helpMessageA ="noahgula.me/heman/help.html";
-}
-
-function help() {
-    return helpMessageA;
-}
-
-function whatsup() {
-    var message = "https://www.youtube.com/watch?v=ZZ5LpwO-An4";
-    return message;
-}
-
-function secret() {
-    var message = cool();
-    return message;
-}
-
-function lion() {
-    var message = "https://www.youtube.com/watch?v=_LBmUwi6mEo";
-    return message;
-}
-
-function channel() {
-    var message = "https://www.youtube.com/watch?v=E9s1ltPGQOo";
-    return message;
-}
-
-function shop() {
-    var message = "https://www.youtube.com/watch?v=gYOEyzBFYa4";
-    return message;
-}
-
-/* function holes() {
-    var num = Math.floor(Math.random() * (3 - 0)) + 0;
-    if(num===0) {
-      var message = ("A straw has no holes.");
-    }
-    else if(num===1) {
-      var message = ("A straw has one hole.");
-    }
-    else if(num===2) {
-      var message = ("A straw has two holes.");
-    }
-    return message;
-} */
-
-function holes() {
-    var message = "I don't even know, man.";
-    return message;
-}
-
-function randomCommand() {
-    var num = Math.floor(Math.random() * (7 - 0)) + 0;
-    if(num===0) {
-        return secret();
-    }
-    else if(num===1) {
-        return whatsup();
-    }
-    else if(num===2) {
-        return lion();
-    }
-    else if(num===3) {     
-        return channel();
-    }
-    else if(num===4) {
-        return shop();
-    }
-    else if(num===5) {
-        return holes();
-    }
-    else if(num===6) {
-        return help()
-    }
-}
-
-
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
-  var botRegex = /Hi Jay!/i; botRegexWhats = /what's going on/i; botRegexLion = /lion sleeps/i; botRegexShop = /Wii shop/i; botRegexChannel = /mii channel/i; botRegexHoles = /How many holes/i; botRegexRandomCommand = /do a thing/i; botRegexLoop = /lol nice try nic/i;
+  var botRegex = /Hi Jay!/i; botRegexHi = /Hi/i; botRegexGot = /Got em/i; botRegexChill = /Chill/i; botRegexOof = /Oof/i; botRegexHoles = /How many holes/i; botRegexRandomCommand = /do a thing/i; botRegexLoop = /lol nice try nic/i;
   
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage("Hi John!");
     this.res.end();
   }
-  else if(request.text && botRegexWhats.test(request.text)) {
+  else if(request.text && botRegexHi.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(whatsup());
+    postMessage("Hey!");
     this.res.end();
   }
-  else if(request.text && botRegexLion.test(request.text)) {
+  else if(request.text && botRegexGot.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(lion());
+    postMessage("You Can't catch me!");
     this.res.end();
   }
-  else if(request.text && botRegexShop.test(request.text)) {
+  else if(request.text && botRegexChill.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(shop());
+    postMessage("I am chill.");
     this.res.end();
   }
-  else if(request.text && botRegexChannel.test(request.text)) {
+  else if(request.text && botRegexOof.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(channel());
+    postMessage("Oofity Oofy!");
     this.res.end();
   }
   else if(request.text && botRegexHoles.test(request.text)) {
