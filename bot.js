27dbc5168f02435419a591a15d0ffe92c5bd6bb9
john35588 <56525183+john35588@users.noreply.github.com>
2020-01-26 18:03:09 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -141,7 +141,7 @@ function respond() {
   }
   else if(request.text && botRegexEcho.test(request.text.toLowerCase())) {
     this.res.writeHead(200);
-    postMessage(request.text);
+    postMessage("");
     this.res.end();
   }
   else {
